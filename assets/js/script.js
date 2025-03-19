454ab979be6a521db2cd67d8b2c31909bde23eee
Wesley de Oliveira <wesleysistemaor@gmail.com>
2025-03-18 23:18:36 -0300

    alt no script do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+const CLIENT_ID = 'SEU_CLIENT_ID_AQUI';
+const API_KEY = 'SEU_API_KEY_AQUI';
+const FOLDER_ID = 'SEU_FOLDER_ID_AQUI';
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+        scope: 'https://www.googleapis.com/auth/drive.file'
+    }).then(() => {
+        gapi.auth2.getAuthInstance().signIn();
+    });
+}
+
 function buscarCidade() {
     const cep = document.getElementById('cep').value;
     if (cep.length === 8) {
@@ -19,38 +38,31 @@ function buscarCidade() {
 
 document.getElementById('cadastroForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const cep = document.getElementById('cep').value;
     const cidade = document.getElementById('cidade').value;
 
     const fileContent = `Nome: ${nome}\nEmail: ${email}\nCEP: ${cep}\nCidade: ${cidade}`;
-    const file = new Blob([fileContent], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
-    const metadata = {
-        'name': 'cadastro.docx',
-        'mimeType': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'parents': ['YOUR_FOLDER_ID'] // Substitua pelo ID da pasta compartilhada
-    };
+    const blob = new Blob([fileContent], { type: 'text/plain' });
 
-    const accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
-    const form = new FormData();
-    form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-    form.append('file', file);
+    gapi.auth2.getAuthInstance().signIn().then(() => {
+        const accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+        const form = new FormData();
+        form.append('metadata', new Blob([JSON.stringify({
+            'name': 'cadastro.txt',
+            'mimeType': 'text/plain',
+            'parents': [FOLDER_ID]
+        })], { type: 'application/json' }));
+        form.append('file', blob);
 
-    fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
-        method: 'POST',
-        headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
-        body: form
-    }).then((response) => {
-        if (response.ok) {
-            alert('Dados salvos com sucesso!');
-        } else {
-            response.json().then((error) => {
-                alert('Erro ao salvar os dados: ' + error.message);
-            });
-        }
-    }).catch((error) => {
-        alert('Erro ao salvar os dados!');
-        console.error(error);
+        fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
+            method: 'POST',
+            headers: { 'Authorization': 'Bearer ' + accessToken },
+            body: form
+        }).then(response => response.json())
+          .then(data => alert('Dados salvos com sucesso!'))
+          .catch(error => console.error('Erro ao salvar os dados:', error));
     });
 });
